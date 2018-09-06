8361b5dfc93540afa4b7dff8a9bf6512fc6f3302
Alexandra Dresco <alexandradresco@MacBook-Pro-9.local>
2018-09-06 14:13:34 +0100

    step 14 - capacity is now 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,21 +2,27 @@ class DockingStation
   attr_reader :bike
 
   def initialize
-    @store = []
+    @bike_capacity = []
   end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-      @bike
+    if @bike_capacity.empty?
+    fail 'No bikes available'
+    else
+      @bike_capacity
+    end
   end
 
   def store
-    @store
+    @bike_capacity
   end
 
   def dock(bike)
-    fail 'Reached capacity' unless @bike.nil?
-    @bike = bike
+    if @bike_capacity.count == 20
+      fail 'Reached capacity'
+    else
+      @bike_capacity << bike
+    end
   end
 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,21 +7,21 @@ describe DockingStation do
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq([bike])
   end
 
   it 'raise another error when reached capacity' do
     bike = Bike.new
     subject.dock(Bike.new)
-    expect {subject.dock(bike)}.to raise_error "Reached capacity"
+    expect {20.times{subject.dock(bike)}}.to raise_error "Reached capacity"
   end
 
   describe '#release_bike' do
-    it 'gives a working bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq(bike)
-      end
+    # it 'gives a working bike' do
+    #   bike = Bike.new
+    #   subject.dock(bike)
+    #   expect(subject.release_bike).to eq(bike_capacity)
+    #   end
 
       it 'raises an error when there is no bike' do
         expect{ subject.release_bike}.to raise_error "No bikes available"
@@ -29,7 +29,7 @@ describe DockingStation do
     end
 
   it "dock bike can store a bike" do
-    expect(subject.dock("first_bike")).to eq("first_bike")
+    expect(subject.dock("first_bike")).to eq(["first_bike"])
   end
 
 end
